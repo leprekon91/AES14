f40d90aaf030a9790ced90c3e578b498eb8af01c
Andrew Grabarnick <reist2009@gmail.com>
2018-06-04 09:34:08 +0300

    Updated to work with the new database module

--- test.sql
+++ test.sql
@@ -2,44 +2,56 @@
 #Comment this two lines out if you already ran the script once
 CREATE DATABASE test
 use test;
-#User table:
-CREATE TABLE `user` (
-  `id` varchar(50) NOT NULL,
-  `password` varchar(50) DEFAULT NULL,
+#Users table:
+CREATE TABLE `users` (
+  `user_name` varchar(50) NOT NULL,
+  `pass` varchar(50) DEFAULT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+  PRIMARY KEY (`user_name`),
+  UNIQUE KEY `user_name_UNIQUE` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-insert into USER (id, password, type) values ('gblankoc', 'Sp9Rd5VV2bsL', 1);
-insert into USER (id, password, type) values ('ecopsey14', 'SELujdmf84', 1);
-insert into USER (id, password, type) values ('yhirche15', 'zGzDU8Dih', 1);
-insert into USER (id, password, type) values ('ekorlat5', 'YU83ueIR', 1);
-insert into USER (id, password, type) values ('hmorrowe7', 'ZdXmMYFI', 1);
-insert into USER (id, password, type) values ('lpoile2', '7nDZbe', 1);
-insert into USER (id, password, type) values ('sboyforde', 'a5EPXj', 1);
-insert into USER (id, password, type) values ('sskrinesi', 'dooQXG9mcRY', 1);
-insert into USER (id, password, type) values ('pnuschkek', 'U5N73TekrsF', 1);
-insert into USER (id, password, type) values ('mkitchingmanl', 'LTMPLNVLMo', 1);
-insert into USER (id, password, type) values ('awhitlandn', 'mLRKVw0mR', 1);
-insert into USER (id, password, type) values ('gnoteyoungo', 'Uf74cS3wxqu', 1);
-insert into USER (id, password, type) values ('phadrillp', 'iXWvm7Drtw', 1);
-insert into USER (id, password, type) values ('ccoulsenq', 'e3WTyRX7', 1);
-insert into USER (id, password, type) values ('shugonett', 'SWjvb9', 1);
-insert into USER (id, password, type) values ('sstapels10', 'Ztv2RXrqU', 1);
+#student user
+INSERT INTO `test`.`users`
+(`user_name`,
+`pass`,
+`first_name`,
+`last_name`,
+`type`)
+VALUES
+('s',
+'s',
+'John',
+'Student',
+1);
 
-insert into USER (id, password, type) values ('jsendall19', 'lyL8tmh0j', 2);
-insert into USER (id, password, type) values ('tlukock3', 'zvuCKvifa', 2);
-insert into USER (id, password, type) values ('bwychard4', 'bjmP4PATmdNa', 2);
-insert into USER (id, password, type) values ('irosen16', 'Up4uKlbAu', 2);
-insert into USER (id, password, type) values ('sdyhouse17', 'HWruJsGp8', 2);
-insert into USER (id, password, type) values ('eaccum8', 'XIEjbn7lu', 2);
-insert into USER (id, password, type) values ('dcosgry9', 'UaadbGciYo2P', 2);
-insert into USER (id, password, type) values ('dsollners', '2xbY876', 2);
-insert into USER (id, password, type) values ('cstidsonu', 'vLSOfCAi', 2);
-insert into USER (id, password, type) values ('dnavarrey', 'kfZqj7bMShdn', 2);
-insert into USER (id, password, type) values ('aclinchz', 'yrz5QOL69bS', 2);
-insert into USER (id, password, type) values ('hfitchet11', 'PspRFPTgedMX', 2);
-insert into USER (id, password, type) values ('kscuffhamw', 'FrTq4fPLPBc', 2);
+#teacher user
+INSERT INTO `test`.`users`
+(`user_name`,
+`pass`,
+`first_name`,
+`last_name`,
+`type`)
+VALUES
+('t',
+'t',
+'Bob',
+'Teacher',
+2);
 
-insert into USER (id, password, type) values ('hmowattx', 'MVUWKrVeX2U', 3);
+#principal user
+INSERT INTO `test`.`users`
+(`user_name`,
+`pass`,
+`first_name`,
+`last_name`,
+`type`)
+VALUES
+('p',
+'p',
+'Alice',
+'Principal',
+3);
