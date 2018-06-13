864d2f5d0133c1950bc1dc83a36bead269f01d05
andrey <reist2009@gmail.com>
2018-06-13 09:41:10 +0300

    Teacher Menu now navigates between two screens

--- src/client/gui/fxcontrol/TeacherMenu.java
+++ src/client/gui/fxcontrol/TeacherMenu.java
@@ -2,6 +2,7 @@ package client.gui.fxcontrol;
 
 import client.control.TeacherControl;
 import com.Contract;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
@@ -14,10 +15,19 @@ public class TeacherMenu {
 
     public TeacherControl teacherControl = TeacherControl.getInstance();
     public AnchorPane questionPane;
+    public AnchorPane examPane;
+    public AnchorPane welcomeScreen;
 
 
     public void initialize() {
         //load question pane
+        loadQuestionPane();
+        loadExamPane();
+        hideAll();
+    }
+
+
+    public void loadQuestionPane() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Contract.clientFXML + "QuestionListView.fxml"));
             Parent root = fxmlLoader.load();
@@ -32,5 +42,40 @@ public class TeacherMenu {
         }
     }
 
+    public void loadExamPane() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Contract.clientFXML + "ExamManagement.fxml"));
+            Parent root = fxmlLoader.load();
+            examPane.getChildren().setAll(root);
+            AnchorPane.setTopAnchor(root, 0.0);
+            AnchorPane.setRightAnchor(root, 0.0);
+            AnchorPane.setLeftAnchor(root, 0.0);
+            AnchorPane.setBottomAnchor(root, 0.0);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void hideAll() {
+        questionPane.setVisible(false);
+        examPane.setVisible(false);
+        welcomeScreen.setVisible(true);
+    }
+
+    public void switchToQuestionView(ActionEvent actionEvent) {
+
+        examPane.setVisible(false);
+        welcomeScreen.setVisible(false);
+        questionPane.setVisible(true);
+
+    }
+
+    public void switchToExamView(ActionEvent actionEvent) {
+
+        questionPane.setVisible(false);
+        welcomeScreen.setVisible(false);
+        examPane.setVisible(true);
+
+    }
 }
