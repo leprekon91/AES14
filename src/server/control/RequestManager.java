a05ed0e0cf252f98f8542decf3b1ca4ca1a3c6fc
leprekon91 <reist2009@gmail.com>
2018-06-15 18:36:52 +0300

    RequestManager class added to server-side

---
+++ src/server/control/RequestManager.java
@@ -0,0 +1,20 @@
+package server.control;
+
+import com.data.ExtensionRequest;
+
+import java.util.ArrayList;
+
+public class RequestManager {
+    private static RequestManager ourInstance = new RequestManager();
+    public ArrayList<ExtensionRequest> requests;
+
+    public static RequestManager getInstance() {
+        return ourInstance;
+    }
+
+
+    private RequestManager() {
+    }
+
+
+}
--- src/server/control/RequestsManager.java
+++
@@ -1,4 +0,0 @@
-package server.control;
-
-public class RequestsManager {
-}
